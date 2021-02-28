f7bdfd17d66ce46a8d5759ec02750665ad5d531d
Eric Ni <en1116@nyu.edu>
2021-02-28 15:04:11 -0500

    Grade C
    Updated the quintic function to a quartic ease out and also made it such that the blob cannot enter the shrinking state while it is already shrinking

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -73,13 +73,7 @@ public class GameController : MonoBehaviour
         }
     }
 
-    // Add and display score.
-    public void AddScore(int scoreToAdd)
-    {
-        score += scoreToAdd;
-        scoreText.text = score.ToString();
-    }
-
+   
     // Remove blob from blob list.
     public void RemoveFromList(Blob blob)
     {
