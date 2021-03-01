8b24d33682a16b87b67fade3ba217044d113d038
TakayaEnomoto <49789142+TakayaEnomoto@users.noreply.github.com>
2021-03-01 20:29:02 +0800

    Loading the assets, and automatically changing format. For BlobStateMoving, fixed the compiler error by adding override into the function. By doing this, the function can override from the derived file instead of using it directly.

--- Assets/Scripts/State/BlobStateMoving.cs
+++ Assets/Scripts/State/BlobStateMoving.cs
@@ -41,7 +41,7 @@ public class BlobStateMoving : BlobState
         }
     }
 
-    public void Enter() // Overriden from base class.
+    public override void Enter() // Overriden from base class.
     {
         base.Enter(); // Call base class.
 
